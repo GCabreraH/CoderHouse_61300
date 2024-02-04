984ab55bb3556751885467b955f4197794589614
GCabreraH <guillermo.cabrera252@gmail.com>
2024-02-04 12:10:30 -0300

    Clase 3: modificaciones para agregar el boton + y dar estilo con stylesheet

--- desarrollo_app_61300/App.js
+++ desarrollo_app_61300/App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Pressable } from 'react-native';
 import Usuario from './src/components/Usuario';
+import cartLogo from './assets/cart.png'
+const imagenCarrito = "https://purepng.com/public/uploads/large/purepng.com-shopping-cartshoppingcarttrolleycarriagebuggysupermarkets-14215265323859lqxv.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Usuarios</Text>
-      <Usuario usuario="Fernando"/>
-      <Text>Chau</Text>
-      
-      <StatusBar style="auto" />
+      <View style={{flexDirection: 'row', justifyContent: "center", gap: 8, alignItems: "center" }}>
+      <Text>Carrito</Text>
+      <Image style={{width: 50, height: 50}} source={{uri: imagenCarrito}} />
+      {/*<Image style={{width: 50, height: 50}} source={cartLogo} />*/}
+      </View>
+
+      <View style={{flexDirection: "row", gap: 30}}>
+        <TextInput style={styles.inputText} placeholder='Ingrese un producto' />
+        <Pressable> 
+        <Text style={{fontSize: 40}}>+</Text>
+        </Pressable>
+      </View>
+
+    <View>
+      <Text style={styles.productos}>Poleras</Text>
+      <Text style={styles.productos}>Pantalon</Text>
+      <Text style={styles.productos}>Gorro</Text>      
+      <Text style={styles.productos}>Poleras</Text>
+      <Text style={styles.productos}>Pantalon</Text>
+      <Text style={styles.productos}>Gorro</Text>      
+      <Text style={styles.productos}>Poleras</Text>
+      <Text style={styles.productos}>Pantalon</Text>
+      <Text style={styles.productos}>Gorro</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  
+  header:{
+  backgroundColor: '#ededed', 
+  flex: 0.16,
+},
+    container: {
     paddingTop: 20,
     paddingHorizontal: 25,
     flex: 1,
@@ -23,4 +49,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  inputText:{
+    borderColor:'red', 
+    borderWidth: 2, 
+    paddingHorizontal: 20, 
+    paddingVertical: 20,
+    borderRadius: 5,
+    width: "80%",
+  },
+
+    productos:{
+    fontSize: 30, 
+    fontWeight: "bold", 
+    padding: 4
+
+  }
+
 });

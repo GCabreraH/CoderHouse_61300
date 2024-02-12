01ae7699677c3b64c34a0a1ecc4e9823b2fc092c
GCabreraH <guillermo.cabrera252@gmail.com>
2024-02-12 20:16:50 -0300

    Creacion del componente RemoveModal con la logica para recibir por props aquellos elementos necesarios para que pueda ser un componente separado de App.js.

---
+++ desarrollo_app_61300/src/components/RemoveModal.jsx
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View, Pressable, Modal} from 'react-native';
+
+const RemoveModal = ({
+modalVisible,
+cartItems,
+setCartItems,
+setModalVisible,
+itemSelected,
+}) => {
+    const removeItem = () => {
+        const fileredArray =cartItems.filter((item) => item.id  !== itemSelected);
+        setCartItems(fileredArray);
+        setModalVisible(false)
+      }; 
+
+    return(
+    <Modal animationType="slide" transparent visible={modalVisible}>
+      <View style={styles.modalContainer}>
+        <Text style={{fontSize: 20}}> ¿Quieres eliminar el producto? </Text>
+        <Pressable onPress={() => setModalVisible(false)}>
+        <Text>No!!</Text>
+        </Pressable>
+        <Pressable onPress={removeItem}>
+        <Text>Sí!</Text>
+        </Pressable>
+      </View>
+    </Modal>
+        );
+    };
+
+  export default RemoveModal;
+
+
+
+  const styles = StyleSheet.create({
+  modalContainer:{
+    height: 200,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    justifyContent: "space-between",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation:5,
+         
+  },
+});

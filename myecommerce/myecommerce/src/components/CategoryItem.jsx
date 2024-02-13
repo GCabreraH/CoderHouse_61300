44bc547c16a0a0676c0bdc86ac5e7b75a4519ebe
GCabreraH <guillermo.cabrera252@gmail.com>
2024-02-12 23:41:39 -0300

    Se agregaron estilos pero no funcionan a CategoryItem.

--- myecommerce/myecommerce/src/components/CategoryItem.jsx
+++ myecommerce/myecommerce/src/components/CategoryItem.jsx
@@ -6,22 +6,28 @@ Lo que este componente renderice se debe pasar por props a Categories.
 import { StyleSheet, Text, View } from 'react-native'
 
 
-const CategoryItem = ({category}) => {
-  return (
-    <View>
-      <Text style={styles.text}>{category}</Text>
-    </View>
-  );
-};
-
-export default CategoryItem
-
-const styles = StyleSheet.create({
-    text:{
-        fontSize: 20,
-
-
-    }
-
-
-})
+const CategoryItem = ({ category }) => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{category}</Text>
+      </View>
+    );
+  };
+  
+  export default CategoryItem;
+  
+  const styles = StyleSheet.create({
+    container: {
+        shadowColor: "#000",
+        shadowOffset:{
+        width: 0,
+        height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    text: {
+      fontSize: 20,
+    },
+  });
